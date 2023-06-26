4d2fbc292fec0e205b8c10bc7370a16fb7cee464
littlefieldnick <nicklittlefield@Nicks-MacBook-Air.local>
2023-06-26 12:54:55 -0500

    Added Track I code files

---
+++ Code/TrackI_ClinicalNLP/stigmatizing_word_list.py
@@ -0,0 +1,10 @@
+STIGMATIZING_WORDS_COMPLETE = ["abuse", "abuser", "abuses", "abusing", "addict", "addicted", "adherence", "adherent",
+                              "alcohol abuse", "argumentative", "been clean", "belligerent", "cheat", "cheating", "cheats",
+                              "combative", "complience", "compliant", "control", "controlled", "controls", "degenerate", 
+                              "depraved", "difficult patient", "drug problem", "drug seeking", "fail", "failed", "fails", 
+                              "failure", "fake", "faking", "habit", "in denial", "junkie", "lifestyle disease", "malinger", 
+                              "malingering", "malingers", "narcotic", "narcotics", "nonadherence", "nonadherent", "noncomplience", 
+                              "noncomplient", "pill problem", "pill seeking", "pot head", "refuse", "refused", "refuses",
+                              "secondary gain", "speedball", "strung out", "substance abuse", "uncontrolled", "unmotivated",
+                              "unwilling", "user"]
+
